bb964a8d3e04f47dcb72f465fc9a7cb91e35861b
Simon Cozens <simon@cpan.org>
2002-08-02 10:40:43 +0000

    Import of SIMON/Shishi-Prototype-0.4 from CPAN.
    gitpan-cpan-distribution: Shishi-Prototype
    gitpan-cpan-version:      0.4
    gitpan-cpan-path:         SIMON/Shishi-Prototype-0.4.tar.gz
    gitpan-cpan-author:       SIMON
    gitpan-cpan-maturity:     released

--- lib/Shishi/Node.pm
+++ lib/Shishi/Node.pm
@@ -17,6 +17,7 @@ my %match = (
     token => sub { my ($d, $tr) = @_; my $tk = chr $d->{token}; $$tr =~ s/^$tk//; },
     any=> sub { my ($d, $tr) = @_; $$tr =~ s/.//; },
     skip=> sub { my ($d, $tr) = @_; $$tr =~ s/.//; },
+    end => sub { my ($d, $tr) = @_; length $$tr == 0; },
     true => sub {1},
     code => sub { my ($d, $tr, $parser) = @_;
         print "Performing code\n" if $Shishi::Debug;
@@ -28,9 +29,14 @@ sub execute {
     my $self = shift;
     my $parser = shift;
     my $match_object = shift;
+    my @decs = @{$self->{decisions}};
+
+    recurse:
+
     print "Executing node $self, parser is $parser, mo is $match_object\n" if $Shishi::Debug;
-    for my $d ($self->decisions) {
+    for my $d (@decs) {
         my $text = $match_object->parse_text();
+        print "This decision is $d\n" if $Shishi::Debug;
         my $targ = $d->{target};
         my $type = $d->{type};
         my $action = $d->{action};
@@ -38,16 +44,36 @@ sub execute {
             if $Shishi::Debug;
         die "Unknown match type $type" unless exists $match{$type};
         next unless $match{$type}->($d, \$text, $parser); # Match
+        print "$type -> $targ succeeded, action $action\n" if $Shishi::Debug;
         $match_object->parse_text($text);
         my $rc;
         if ($action == ACTION_CONTINUE) {
-           $rc = $d->{next_node}->execute($parser, $match_object);
-           return $rc unless $rc == 0;
-        } elsif ($self->{action} == ACTION_FINISH) {
+           # Put stuff on stack.
+           print "Matched, continuing, recursing\n" if $Shishi::Debug;
+           push @{$match_object->{been}}, { node => $self, text => $text, d => $d};
+           $self = $d->{next_node};
+           @decs = @{$self->{decisions}};
+           goto recurse;
+        } elsif ($action == ACTION_FINISH) {
+            print "Finishing\n" if $Shishi::Debug;
            return 1;
-        } elsif ($self->{action} == ACTION_FAIL) {
+        } elsif ($action == ACTION_FAIL) {
+           print "Bailing!\n" if $Shishi::Debug;
            return -1;
         }
+    }
+    print "I need to pop the stack here at end\n" if $Shishi::Debug;
+    if (my $pframe = pop @{$match_object->{been}}) {
+        $self = $pframe->{node};
+        my $text = $pframe->{text};
+        $match_object->parse_text($text);
+        @decs = $self->decisions;
+        while (1) {
+            die "Internal error: decision not found" unless @decs;
+            my $x = shift @decs;
+            last if $x == $pframe->{d};
+        }
+        goto recurse;
     }
     return 0;
 }
